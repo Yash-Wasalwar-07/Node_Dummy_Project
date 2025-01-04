e15f8695ae1207fa6a926a120400c337178f3324
Yash Wasalwar <yash.wasalwar07@hotmail.com>
2025-01-04 16:00:28 +0530

    MongoDB online server and env vars set

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 const Person = require('./models/Person');
 const MenuItem = require('./models/Menu');
@@ -17,6 +20,6 @@ const menuitemRoutes = require('./routes/menuitemRoutes');
 app.use('/person', personRoutes);
 app.use('/menuitem', menuitemRoutes);
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('Server is running on port 3000');
 });
